3d12aa9e880ddc86f5b3f0df02fe71fe8d426979
Ahmed Hisham <Ahmed.Hisham@tpay.me>
2022-04-03 18:01:39 +0200

    Employee add changes

--- src/app/modules/employees/components/add-employee/add-employee.component.ts
+++ src/app/modules/employees/components/add-employee/add-employee.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { TuiContextWithImplicit, tuiPure } from "@taiga-ui/cdk";
 
 import { TuiCountryIsoCode } from "@taiga-ui/i18n";
+import { personType } from "src/app/shared/models/person.model";
 
 @Component({
   selector: "app-add-employee",
@@ -13,7 +15,8 @@ export class AddEmployeeComponent implements OnInit {
   createNew = new FormGroup({
     firstName: new FormControl("", Validators.required),
     lastName: new FormControl("", Validators.required),
-    // type: personType;
+    // will add drop down for later 
+    type: new FormControl(personType.DOCTOR,Validators.required),
     email: new FormControl("", [Validators.required, Validators.email]),
     // countryCode: new FormControl('', Validators.required),
     mobileNumber: new FormControl("", Validators.required),
@@ -23,6 +26,15 @@ export class AddEmployeeComponent implements OnInit {
   countryIsoCode = TuiCountryIsoCode.EG;
 
   createLoading: boolean = false;
+  types = [{
+    id: personType.DOCTOR,
+    name: 'Doctor',
+  },
+  {
+    id: personType.EMPLOYEE,
+    name: 'Employee',
+  }
+];
   constructor() {}
 
   ngOnInit(): void {}
@@ -33,5 +45,11 @@ export class AddEmployeeComponent implements OnInit {
       this.createLoading = false;
       this.openSidebar.emit(this.createNew.value);
     }, 3000);
+  }
+    @tuiPure
+  stringify(types:  { id: personType; name: string }[]): any {
+    const map = new Map(types.map(({ id, name }) => [id, name] as [personType, string]));
+
+    return ({ $implicit }: TuiContextWithImplicit<any>) => map.get($implicit) || "";
   }
 }
