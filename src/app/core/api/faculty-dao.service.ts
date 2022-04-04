7b22bad394f87a55adb397b54a689585be78d15d
Waleed Rostom <waleedrostom@gmail.com>
2022-04-04 13:54:11 +0200

    add type for facultyDAO

--- src/app/core/api/faculty-dao.service.ts
+++ src/app/core/api/faculty-dao.service.ts
@@ -1,12 +1,14 @@
 import { Inject, Injectable } from "@angular/core";
 
+import { Faculty } from "src/app/shared/models/faculty.model";
+
 import { ApiService } from "./api.service";
 import { GlobalDAOService } from "./global-dao.service";
 
 @Injectable({
   providedIn: "root",
 })
-export class FacultyDAOService extends GlobalDAOService<any> {
+export class FacultyDAOService extends GlobalDAOService<Faculty> {
   constructor(api: ApiService) {
     super("faculty", api);
   }
