aac89755a204a2e147afa7e93119e6e87f4bec10
Waleed Rostom <waleedrostom@gmail.com>
2022-04-05 22:53:38 +0200

    add calendar controls

--- src/app/shared/calendar/calendar.module.ts
+++ src/app/shared/calendar/calendar.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
+import { TuiGroupModule } from "@taiga-ui/core";
 import { CalendarModule, DateAdapter } from "angular-calendar";
 import { adapterFactory } from "angular-calendar/date-adapters/date-fns";
 
+import { SharedModule } from "../shared.module";
 import { CalendarComponent } from "./calendar.component";
 
 @NgModule({
@@ -14,7 +16,9 @@ import { CalendarComponent } from "./calendar.component";
       provide: DateAdapter,
       useFactory: adapterFactory,
     }),
+    TuiGroupModule,
+    SharedModule,
   ],
-  exports: [CalendarComponent],
+  exports: [CalendarComponent, TuiGroupModule],
 })
 export class CalendarECLModule {}
